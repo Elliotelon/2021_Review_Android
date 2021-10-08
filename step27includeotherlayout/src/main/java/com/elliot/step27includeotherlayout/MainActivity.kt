3a95539373835cba7e3647b5522dd3b67bd9ffa2
kimminkyu <kmk50009@naver.com>
2021-10-09 01:16:22 +0900

    Review Step27IncludeOtherLayout

---
+++ step27includeotherlayout/src/main/java/com/elliot/step27includeotherlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.elliot.step27includeotherlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
